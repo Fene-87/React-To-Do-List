ea6c7bbc168af77d23c0f69fb75436278d3ba086
Fene-87 <fenekayas87@gmail.com>
2023-02-23 11:39:37 +0300

    Configure linters files then fix linters errors

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,39 +1,38 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const InputTodo = ({ addTodoItem }) => {
-    const [title, setTitle] = useState('');
-    const [message, setMessage] = useState('');
+  const [title, setTitle] = useState('');
+  const [message, setMessage] = useState('');
 
-    const handleChange = (e) => {
-        setTitle(e.target.value);
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (title.trim()) {
-            addTodoItem(title)
-            setTitle('');
-            setMessage('');
-        } else {
-            setMessage('please add item')
-        }
-    }
-
-    return (
-        <>
-            <form onSubmit={handleSubmit} className="form-container">
-                <input 
-                  type="text" 
-                  placeholder="Add Todo..." 
-                  value={title}
-                  onChange={handleChange}
-                  className="input-text"
-                />
-                <button className="input-submit">Submit</button>
-            </form>
-            <span className="submit-warning">{message}</span>
-        </>
-    )
+  const handleChange = (e) => {
+    setTitle(e.target.value);
   };
-  export default InputTodo;
-  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim()) {
+      addTodoItem(title);
+      setTitle('');
+      setMessage('');
+    } else {
+      setMessage('please add item');
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="form-container">
+        <input
+          type="text"
+          placeholder="Add Todo..."
+          value={title}
+          onChange={handleChange}
+          className="input-text"
+        />
+        <button type="submit" className="input-submit">Submit</button>
+      </form>
+      <span className="submit-warning">{message}</span>
+    </>
+  );
+};
+export default InputTodo;
